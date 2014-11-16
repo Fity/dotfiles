dc6bd1c5ffa4dc13cd0e917d0889ec5b1bbaea54
Fity <imfity@gmail.com>
2014-11-16 21:40:03 +0800

    add alias.sh

---
+++ alias.sh
@@ -0,0 +1,8 @@
+# cli alias
+alias purl='http'
+# use tig instead
+# alias gl="git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias crontab="VIM_CRONTAB=true crontab"
+
+# git alias
+alias ga='git add -A'
